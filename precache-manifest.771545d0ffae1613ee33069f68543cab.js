94b1f51c6e0fc86d70ea1b2f301e826b0f62a728
Andrew Leedham <AndrewLeedham@Outlook.com>
2020-04-17 19:54:27 +0100

    Updates

---
+++ precache-manifest.771545d0ffae1613ee33069f68543cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b4ba043718f256fcca22176815bd19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89561210177cfe1c8b64",
+    "url": "./static/css/main.c778b8b1.chunk.css"
+  },
+  {
+    "revision": "ff098bab9c2b1bc90955",
+    "url": "./static/js/2.9d0e6fe8.chunk.js"
+  },
+  {
+    "revision": "25f65529c59b5bb2370ba7566efd57ed",
+    "url": "./static/js/2.9d0e6fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89561210177cfe1c8b64",
+    "url": "./static/js/main.0b0cfa87.chunk.js"
+  },
+  {
+    "revision": "f1c94fa862e5fd7a4b7c",
+    "url": "./static/js/runtime-main.22732639.js"
+  }
+]);
