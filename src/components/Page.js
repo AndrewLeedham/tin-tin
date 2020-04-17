24695d3459ccbafb41a00f0a710b2db4430bfd9b
Andrew Leedham <AndrewLeedham@Outlook.com>
2020-04-18 00:02:50 +0100

    fix: padding for mobile

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,7 +3,7 @@ import { Heading, Text, Box } from "@chakra-ui/core";
 
 export default function Page({ heading, subHeading, children }) {
   return (
-    <Box my="10" maxW="md" mx="auto">
+    <Box my="10" maxW="md" mx="auto" px="4">
       {heading && (
         <Heading as="h1" mb={2}>
           {heading}
